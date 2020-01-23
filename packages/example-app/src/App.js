0db7e943d6cb99962fec87f04b20c11e5efa33eb
Séverine Darlot <severine.darlot@hiptest.net>
2020-01-23 12:52:26 +0100

    Create an object from the list of file path

--- packages/example-app/src/App.js
+++ packages/example-app/src/App.js
@@ -3,21 +3,30 @@ import logo from './logo.svg'
 import './App.css'
 import { Tree } from "@blueprintjs/core"
 
-function generateTree() {
+function listFiles() {
   return [
-            { id: "1", label: "one.js", icon: "document" },
-            { id: "2", label: "two.js", icon: "document" },
-            { id: "3", label: "a-folder", icon: "folder-open", isExpanded: true, childNodes: [
-              { id: "4", label: "three.js", icon: "document"}
-            ]}
-          ]
+    "one.js", "two.js", "a-folder/three.js"
+  ]
+}
+
+
+
+function generateTree(paths) {
+  return paths.map((path, index) => ({ id: index, label: path, icon: "document" }))
+  // return [
+  //           { id: "1", label: "one.js", icon: "document" },
+  //           { id: "2", label: "two.js", icon: "document" },
+  //           { id: "3", label: "a-folder", icon: "folder-open", isExpanded: true, childNodes: [
+  //             { id: "4", label: "three.js", icon: "document"}
+  //           ]}
+  //         ]
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Tree contents={generateTree()}></Tree>
+        <Tree contents={generateTree(listFiles())}></Tree>
       </header>
     </div>
   );
