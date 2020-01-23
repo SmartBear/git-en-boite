51ce834782e99841131d2a3e292c024323d4cb47
Séverine Darlot <severine.darlot@hiptest.net>
2020-01-23 17:24:44 +0100

    Use HTMLSelect of blueprint

--- packages/example-app/src/App.js
+++ packages/example-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { Tree, Button } from "@blueprintjs/core"
+import { Tree, Button, HTMLSelect } from "@blueprintjs/core"
 
 const generateTree = (paths) =>
   paths.map(({ id, attributes: { path } }) => ({ id, label: path, icon: "document" }))
@@ -18,9 +18,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <select >
-          {['master', 'sprint1', 'sprint2'].map((name) => <option>{name}</option>)}
-        </select>
+        <HTMLSelect >
+          {['master', 'sprint1', 'sprint2'].map((name, index) => <option key={index}>{name}</option>)}
+        </HTMLSelect>
         <Tree contents={generateTree(files)}/>
       </header>
     </div>
