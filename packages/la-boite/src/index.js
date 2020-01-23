ccb9dcbc6d08aaf44535ec41e7a275aa78c6b24d
Benjamin Lethier <benjamin.lethier@hiptest.net>
2020-01-23 16:39:15 +0100

    Display file path and handles directories

--- packages/la-boite/src/index.js
+++ packages/la-boite/src/index.js
@@ -3,16 +3,35 @@ const Router = require("koa-router");
 const cors = require('koa2-cors');
 const Git = require("nodegit");
 
-const app = new Koa();
-const router = new Router();
+const app = new Koa()
+const router = new Router()
+
+
+
+const walkTree = (tree, filePaths = []) => {
+  const treeWalker = tree.walk()
+  const files = []
+
+  return new Promise(resolve => {
+    treeWalker.on('entry', function(entry) {
+      files.push(entry.path())
+    })
+    treeWalker.on('end', function(trees) {
+      resolve(files)
+    })
+
+    treeWalker.start()
+  })
+}
 
 const getFiles = async () => {
   const repository = await Git.Repository.open('repository')
   const masterCommit = await repository.getMasterCommit()
-  const entries = (await masterCommit.getTree()).entries()
-  return entries.map(entry => entry.path())
-}
+  const tree = await masterCommit.getTree()
+  const files = await walkTree(tree)
 
+  return files
+}
 
 const serializedListOfFiles = async () => {
   const listOfFiles = await getFiles()
@@ -27,10 +46,10 @@ const serializedListOfFiles = async () => {
   };
 };
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: '*' }))
 
 router.get("/files", async (ctx, next) => {
-  ctx.body = await serializedListOfFiles();
+  ctx.body = await serializedListOfFiles()
 });
 
 app.use(router.routes()).use(router.allowedMethods());
