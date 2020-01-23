fd1874a761b06c74d7f4c0cdf7e577ac89faa023
Romain Gérard <romaingweb@gmail.com>
2020-01-23 21:24:25 +0100

    Extract Repo

--- packages/la-boite/src/index.js
+++ packages/la-boite/src/index.js
@@ -1,78 +1,44 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const cors = require('koa2-cors')
-const Git = require('nodegit')
+const Repo = require('./repo')
 
 const app = new Koa()
 const router = new Router()
 
-const walkTree = (tree) => {
-  const treeWalker = tree.walk()
-  const files = []
+const serializedListOfFiles = files => ({
+  data: files.map((file, index) => ({
+    type: 'file',
+    id: index,
+    attributes: {
+      path: file
+    }
+  }))
+})
 
-  return new Promise(resolve => {
-    treeWalker.on('entry', function (entry) {
-      files.push(entry.path())
-    })
-    treeWalker.on('end', function () {
-      resolve(files)
-    })
-
-    treeWalker.start()
-  })
-}
-
-const getFiles = async (branchName = 'master') => {
-  const repository = await Git.Repository.open('repository')
-  const commit = await repository.getReferenceCommit(branchName)
-  const tree = await commit.getTree()
-  const files = await walkTree(tree)
-
-  return files
-}
-
-const serializedListOfFiles = async (branchName = 'master') => {
-  const listOfFiles = await getFiles(branchName)
-  return {
-    data: listOfFiles.map((file, index) => ({
-      type: 'file',
-      id: index,
-      attributes: {
-        path: file
-      }
-    }))
-  }
-}
-
-const getBranches = async () => {
-  const repository = await Git.Repository.open('repository')
-  const referenceNames = await repository.getReferenceNames(Git.Reference.TYPE.LISTALL)
-  return referenceNames;
-}
-
-const serializedListOfBranches = async () => {
-  const branches = await getBranches()
-  return {
-    data: branches.map((branch, index) => ({
-      type: 'branch',
-      id: index,
-      attributes: {
-        name: branch
-      }
-    }))
-  }
-}
+const serializedListOfBranches = branches => ({
+  data: branches.map((branch, index) => ({
+    type: 'branch',
+    id: index,
+    attributes: {
+      name: branch
+    }
+  }))
+})
 
 app.use(cors({ origin: '*' }))
 
-router.get('/files', async (ctx, next) => {
-  ctx.body = await serializedListOfFiles()
+router.get('/files', async ctx => {
+  const files = await new Repo().getFiles('master')
+  ctx.body = serializedListOfFiles(files)
 })
-router.get('/files/:branch*', async (ctx, next) => {
-  ctx.body = await serializedListOfFiles(ctx.params.branch)
+router.get('/files/:branch*', async ctx => {
+  const files = await new Repo().getFiles(ctx.params.branch)
+  ctx.body = serializedListOfFiles(files)
 })
-router.get('/branches', async (ctx, next) => {
-  ctx.body = await serializedListOfBranches()
+router.get('/branches', async ctx => {
+  const branches = await new Repo().getBranches()
+  ctx.body = serializedListOfBranches(branches)
 })
 
 app.use(router.routes()).use(router.allowedMethods())
---
+++ packages/la-boite/src/repo.js
@@ -0,0 +1,35 @@
+const Git = require('nodegit')
+
+module.exports = class Repo {
+  constructor() {
+    this._repoName = 'repository'
+  }
+
+  async getBranches() {
+    const repository = await Git.Repository.open('repository')
+    return repository.getReferenceNames(Git.Reference.TYPE.LISTALL)
+  }
+
+  async getFiles(branchName = 'master') {
+    const repository = await Git.Repository.open(this._repoName)
+    const commit = await repository.getReferenceCommit(branchName)
+    const tree = await commit.getTree()
+    return walkTree(tree)
+  }
+}
+
+const walkTree = (tree) => {
+  const treeWalker = tree.walk()
+  const files = []
+
+  return new Promise(resolve => {
+    treeWalker.on('entry', function (entry) {
+      files.push(entry.path())
+    })
+    treeWalker.on('end', function () {
+      resolve(files)
+    })
+
+    treeWalker.start()
+  })
+}
