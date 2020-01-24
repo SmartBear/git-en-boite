0eb82ae2837c2a0d546dbd720200a545a25377cd
Karam EL HABOUTI <karam.elhabouti@smartbear.com>
2020-01-24 11:09:58 +0100

    Add koa-bodyparser

--- packages/la-boite/src/index.js
+++ packages/la-boite/src/index.js
@@ -1,9 +1,11 @@
 const Koa = require('koa')
 const cors = require('koa2-cors')
 const router = require('./router')
+const bodyParser = require('koa-bodyparser')
 
 const app = new Koa()
 
+app.use(bodyParser())
 app.use(cors({ origin: '*' }))
 app.use(router.routes()).use(router.allowedMethods())
 
