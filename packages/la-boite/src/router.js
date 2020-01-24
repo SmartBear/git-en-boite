dd7763e4888b47a65914cc292eca7ff327ce9fca
Karam EL HABOUTI <karam.elhabouti@smartbear.com>
2020-01-24 11:10:30 +0100

    Add endpoint for receiving GitHub webhooks

--- packages/la-boite/src/router.js
+++ packages/la-boite/src/router.js
@@ -8,13 +8,19 @@ router.get('/files', async ctx => {
   const files = await new Repo().getFiles('master')
   ctx.body = serializedListOfFiles(files)
 })
+
 router.get('/files/:branch*', async ctx => {
   const files = await new Repo().getFiles(ctx.params.branch)
   ctx.body = serializedListOfFiles(files)
 })
+
 router.get('/branches', async ctx => {
   const branches = await new Repo().getBranches()
   ctx.body = serializedListOfBranches(branches)
+})
+
+router.post('/github/webhooks', async ctx => {
+  ctx.status = 200
 })
 
 module.exports = router
