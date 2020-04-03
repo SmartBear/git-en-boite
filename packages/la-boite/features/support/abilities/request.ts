109f94aa661ae95f05e04017f29e6f9e0960f573
Matt Wynne <matt@mattwynne.net>
2020-04-03 16:45:57 +0100

    Remove cruft

--- packages/la-boite/features/support/abilities/request.ts
+++ packages/la-boite/features/support/abilities/request.ts
@@ -1,13 +1,11 @@
 import request from 'supertest'
 import { create } from '../../../src/web_app'
 import { Before, After } from 'cucumber'
-import { LocalGitRepos } from '../../../src/repos/local_git_repos'
 import { Server } from 'http'
 
 let webServer: Server
 
 Before(function () {
-  this.app = new LocalGitRepos()
   const webApp = create(this.app)
   webServer = webApp.listen(8888)
   this.request = request(webServer)
