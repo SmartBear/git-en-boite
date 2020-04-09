7a06c859706a6d69d19af7e0f59eebb8193ad2b6
Test User <test@example.com>
2020-04-09 16:15:50 +0100

    Add web request logging

--- packages/la-boite/src/web_app.ts
+++ packages/la-boite/src/web_app.ts
@@ -1,5 +1,6 @@
 import Koa from 'koa'
 import cors from 'koa2-cors'
+import logger from 'koa-log'
 import Router from './router'
 import bodyParser from 'koa-bodyparser'
 import { GitRepos } from './repos/git_repos'
@@ -7,6 +8,7 @@ import { GitRepos } from './repos/git_repos'
 function create(app: GitRepos): Koa {
   const webApp = new Koa()
   const router = Router.create(app)
+  webApp.use(logger('combined'))
   webApp.use(bodyParser())
   webApp.use(cors({ origin: '*' }))
   webApp.use(router.routes()).use(router.allowedMethods())
