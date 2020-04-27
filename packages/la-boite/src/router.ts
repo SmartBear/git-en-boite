bcd7ee99e26b9fd562da24f99163f27ef6bafc36
Matt Wynne <matt@mattwynne.net>
2020-04-27 22:31:02 +0100

    Renove nodegit because it's being a pain

--- packages/la-boite/src/router.ts
+++ packages/la-boite/src/router.ts
@@ -1,7 +1,5 @@
 import { Context } from 'koa'
 import Router from 'koa-router'
-import { Repo } from './repo'
-import { serializedListOfBranches, serializedListOfFiles } from './serializers'
 import ReposRouter from './repos/repos_router'
 import { createConfig } from './config'
 import { Application } from './application'
@@ -15,37 +13,6 @@ function create(app: Application): Router {
   })
 
   router.use(ReposRouter.create(app).routes())
-
-  router.get('/files', async (ctx: Context) => {
-    const files: string[] = await new Repo().getFiles('master')
-    ctx.body = serializedListOfFiles(files)
-  })
-
-  router.get('/files/:branch*', async (ctx: Context) => {
-    const files = await new Repo().getFiles(ctx.params.branch)
-    ctx.body = serializedListOfFiles(files)
-  })
-
-  router.get('/branches', async (ctx: Context) => {
-    const branches = await new Repo().getBranches()
-    ctx.body = serializedListOfBranches(branches)
-  })
-
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const SseStream = require('ssestream')
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let sse: any
-  router.post('/github/webhooks', async (ctx: Context) => {
-    await new Repo().pullFromOrigin()
-    sse.write({ event: 'repository-updated', data: 'well done' })
-    ctx.status = 200
-  })
-
-  router.get('/sse', (ctx: Context) => {
-    sse = new SseStream(ctx.req)
-    sse.pipe(ctx.res)
-    ctx.body = sse
-  })
 
   return router
 }
