43d3b8a81301602adad4a7580bc21cd363b0ab68
Julien Biezemans <test@example.com>
2020-05-20 11:49:46 +0200

    Consistently use arrow functions in handlers

--- packages/adapter-git/src/handlers/handleFetch.ts
+++ packages/adapter-git/src/handlers/handleFetch.ts
@@ -2,6 +2,6 @@ import { Fetch } from 'git-en-boite-core-port-git'
 
 import { Handler } from './handler'
 
-export const handleFetch: Handler<Fetch> = async function handleFetch(repo) {
+export const handleFetch: Handler<Fetch> = async repo => {
   await repo.execGit('fetch', 'origin')
 }
--- packages/adapter-git/src/handlers/handleSetOrigin.ts
+++ packages/adapter-git/src/handlers/handleSetOrigin.ts
@@ -2,6 +2,6 @@ import { SetOrigin } from 'git-en-boite-core-port-git'
 
 import { Handler } from './handler'
 
-export const handleSetOrigin: Handler<SetOrigin> = async function handleSetOrigin(repo, { url }) {
+export const handleSetOrigin: Handler<SetOrigin> = async (repo, { url }) => {
   await repo.execGit('remote', 'add', 'origin', url)
 }
