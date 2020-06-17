60de3bd773cdf2bdedc2e6a9da9db05c809c5b27
Julien Biezemans <test@example.com>
2020-06-17 10:26:08 +0200

    Move Cucumber CLI flags to profiles

--- packages/app/cucumber.js
+++ packages/app/cucumber.js
@@ -1,4 +1,6 @@
+const COMMON_FLAGS = "--require-module ts-node/register --require 'features/**/*.ts'"
+
 module.exports = {
-  default: '--tags "not @wip" --format ../../node_modules/cucumber-pretty',
-  wip: '--tags @wip --format ../../node_modules/cucumber-pretty'
+  default: `${COMMON_FLAGS} --tags "not @wip" --format ../../node_modules/cucumber-pretty`,
+  wip: `${COMMON_FLAGS} --tags @wip --format ../../node_modules/cucumber-pretty`,
 }
