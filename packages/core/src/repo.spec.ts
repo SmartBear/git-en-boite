3e55f84d4ca73a5b5df4cae080eff99f5cb496e7
Julien Biezemans <jb@jbpros.com>
2020-06-26 12:10:31 +0100

    Refine tests around Repo model

--- packages/core/src/repo.spec.ts
+++ packages/core/src/repo.spec.ts
@@ -1,4 +1,4 @@
-import { messageDispatch, Dispatch } from 'git-en-boite-command-bus'
+import { AsyncCommand, AsyncQuery, Handle, messageDispatch } from 'git-en-boite-command-bus'
 import {
   BareRepoProtocol,
   Connect,
@@ -9,7 +9,7 @@ import {
   SetOrigin,
   ValidateRemote,
 } from 'git-en-boite-git-port'
-import { assertThat, equalTo } from 'hamjest'
+import { assertThat, equalTo, fulfilled, isRejectedWith, promiseThat } from 'hamjest'
 import sinon from 'sinon'
 
 import { Ref, Repo } from '.'
@@ -18,19 +18,58 @@ describe(Repo.name, () => {
   context('handling a query for the latest Refs', () => {
     it('queries the git repo and returns (a promise of) the result', async () => {
       const expectedRefs = [new Ref('a-revision', 'a-branch')]
-      const fakeGit = messageDispatch<BareRepoProtocol>().withHandlers({}, [
-        [Connect, sinon.stub().resolves()],
-        [Fetch, sinon.stub()],
-        [Init, sinon.stub()],
-        [SetOrigin, sinon.stub()],
-        [ValidateRemote, sinon.stub()],
-        [GetRefs, sinon.stub().resolves(expectedRefs)],
-        [GetConfig, sinon.stub()],
-      ])
-      const repo = new Repo('a-repo-id', fakeGit)
+      const gitRepo = fakeGitRepo({
+        handleConnect: sinon.stub().resolves(),
+        handleGetRefs: sinon.stub().resolves(expectedRefs),
+      })
+      const repo = new Repo('a-repo-id', gitRepo)
       await repo.connect('a-remote-url')
       const refs = await repo.getRefs()
       assertThat(refs, equalTo(expectedRefs))
     })
   })
+
+  context('connecting', () => {
+    it('returns as soon as the git command has completed', () => {
+      let finishGitConnect: () => void
+      const gitConnectResult = new Promise(resolve => {
+        finishGitConnect = resolve
+      })
+      const gitRepo = fakeGitRepo({ handleConnect: sinon.stub().returns(gitConnectResult) })
+      const repo = new Repo('a-repo-id', gitRepo)
+      const connecting = repo.connect('a-remote-url')
+      finishGitConnect()
+      promiseThat(connecting, fulfilled())
+    })
+
+    it('rejects if the git command fails', () => {
+      const gitRepo = fakeGitRepo({
+        handleConnect: sinon.stub().rejects(new Error('Unable to connect')),
+      })
+      const repo = new Repo('a-repo-id', gitRepo)
+      promiseThat(repo.connect('a-bad-url'), isRejectedWith(new Error('Unable to connect')))
+    })
+  })
 })
+
+const fakeGitRepo = (
+  {
+    handleConnect,
+    handleGetRefs,
+  }: {
+    handleConnect?: Handle<unknown, AsyncCommand<Connect>>
+    handleGetRefs?: Handle<unknown, AsyncQuery<GetRefs, Ref[]>>
+  } = {
+    handleConnect: sinon.stub(),
+    handleGetRefs: sinon.stub(),
+  },
+) =>
+  messageDispatch<BareRepoProtocol>().withHandlers({}, [
+    [Connect, handleConnect],
+    [Fetch, sinon.stub()],
+    [Init, sinon.stub()],
+    [SetOrigin, sinon.stub()],
+    [ValidateRemote, sinon.stub()],
+    [GetRefs, handleGetRefs],
+    [GetConfig, sinon.stub()],
+  ])
