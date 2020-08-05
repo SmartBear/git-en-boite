5fdecd1bc4f2b893c68a4b006c26117505009a24
Matt Wynne <matt@mattwynne.net>
2020-08-05 15:48:42 +0100

    Add back old handleCommit

---
+++ packages/local-git/src/handlers/handleCommitToNonBareRepo.ts
@@ -0,0 +1,12 @@
+import { AsyncCommand, Handle } from 'git-en-boite-message-dispatch'
+import { Commit } from '../operations'
+import { GitDirectory } from '../git_directory'
+
+export const handleCommit: Handle<GitDirectory, AsyncCommand<Commit>> = async (
+  repo,
+  { message, author },
+) => {
+  await repo.execGit('commit', ['--allow-empty', '-m', message], {
+    env: { GIT_AUTHOR_NAME: author.name, GIT_AUTHOR_EMAIL: author.email },
+  })
+}
