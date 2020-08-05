69ebd0dcb1e9954970898cfe28840137ba5a52a1
Matt Wynne <matt@mattwynne.net>
2020-08-05 17:01:53 +0100

    Mark in-progress test as @wip

--- packages/local-git/src/handlers/handleCommitToBareRepo.spec.ts
+++ packages/local-git/src/handlers/handleCommitToBareRepo.spec.ts
@@ -61,7 +61,7 @@ describe('handleCommitToBareRepo', () => {
         await git(Fetch.fromOrigin())
       })
 
-      it('creates a commit with the given message', async () => {
+      it('creates an empty commit with the given message', async () => {
         await git(Commit.withMessage('A commit message'))
         await promiseThat(
           exec('git log main --oneline', { cwd: repoPath }),
@@ -69,7 +69,7 @@ describe('handleCommitToBareRepo', () => {
         )
       })
 
-      xit('creates a commit containing the given files', async () => {
+      it.skip('@wip creates a commit containing the given files', async () => {
         const file = { path: 'a.file', content: 'some content' }
         const branchName = 'a-branch'
         await git(Commit.newFile(file).toBranch(branchName))
