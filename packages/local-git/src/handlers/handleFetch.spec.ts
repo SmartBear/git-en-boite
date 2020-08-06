8b56509ad1fcff437e3b23ea3ea94504e51c2868
Romain Gérard <romaingweb@gmail.com>
2020-08-06 17:22:28 +0200

    Use bare repos in handleFetch spec
    Co-authored-by: Julien Biezemans <jb@jbpros.com>

--- packages/local-git/src/handlers/handleFetch.spec.ts
+++ packages/local-git/src/handlers/handleFetch.spec.ts
@@ -1,7 +1,5 @@
-import childProcess from 'child_process'
 import fs from 'fs'
-import { AsyncCommand, messageDispatch } from 'git-en-boite-message-dispatch'
-import { Commit, Fetch, GetRevision, Init, SetOrigin } from '../operations'
+import { AsyncCommand, Dispatch, messageDispatch } from 'git-en-boite-message-dispatch'
 import {
   assertThat,
   equalTo,
@@ -13,81 +11,72 @@ import {
 } from 'hamjest'
 import path from 'path'
 import { dirSync } from 'tmp'
-import { promisify } from 'util'
 
-import { NonBareRepoFactory } from '..'
+import { BareRepoFactory } from '..'
 import { GitDirectory } from '../git_directory'
+import { Commit, Fetch, GetRevision, Init, SetOrigin } from '../operations'
 import { handleFetch } from './handleFetch'
 import { handleInit } from './handleInit'
 import { handleSetOrigin } from './handleSetOrigin'
 
-const exec = promisify(childProcess.exec)
-
 type Protocol = [AsyncCommand<Init>, AsyncCommand<SetOrigin>, AsyncCommand<Fetch>]
 
 describe('handleFetch', () => {
+  const branchName = 'main'
   let root: string
+  let latestCommit: string
+  let originUrl: string
+  let git: Dispatch<Protocol>
+  let repo: GitDirectory
+  let repoPath: string
 
-  beforeEach(() => (root = dirSync().name))
+  beforeEach(async () => {
+    root = dirSync().name
+    repoPath = path.resolve(root, 'a-repo-id')
+    originUrl = path.resolve(root, 'remote', 'a-repo-id')
+
+    const origin = await new BareRepoFactory().open(originUrl)
+    await origin(Commit.withAnyMessage().toBranch(branchName))
+    latestCommit = await origin(GetRevision.forBranchNamed(branchName))
+    fs.mkdirSync(repoPath, { recursive: true })
+    repo = new GitDirectory(repoPath)
+    git = messageDispatch<Protocol>().withHandlers(repo, [
+      [Init, handleInit],
+      [SetOrigin, handleSetOrigin],
+      [Fetch, handleFetch],
+    ])
+    await git(Init.bareRepo())
+  })
+
   afterEach(function () {
     if (this.currentTest.state === 'failed' && this.currentTest.err)
       this.currentTest.err.message = `\nFailed using tmp directory:\n${root}\n${this.currentTest.err?.message}`
   })
 
-  context('an origin repo with commits on master', () => {
-    let latestCommit: string
-    let originUrl: string
+  it('fetches the lastest commit from the origin remote', async () => {
+    await git(SetOrigin.toUrl(originUrl))
+    await git(Fetch.fromOrigin())
+    const { stdout } = await repo.execGit('rev-parse', [`refs/remotes/origin/${branchName}`])
+    assertThat(stdout, startsWith(latestCommit))
+  })
 
-    beforeEach(async () => {
-      originUrl = path.resolve(root, 'remote', 'a-repo-id')
-      const origin = await new NonBareRepoFactory().open(originUrl)
-      await origin(Commit.withAnyMessage())
-      latestCommit = await origin(GetRevision.forBranchNamed('master'))
-    })
+  it('fetches only 1 commit from the origin remote', async () => {
+    await git(SetOrigin.toUrl(originUrl))
+    await git(Fetch.fromOrigin())
+    const { stdout } = await repo.execGit('rev-list', [
+      '--count',
+      `refs/remotes/origin/${branchName}`,
+    ])
+    assertThat(stdout.trim(), equalTo('1'))
+  })
 
-    const openRepo = (repoPath: string) => {
-      fs.mkdirSync(repoPath, { recursive: true })
-      const repo = new GitDirectory(repoPath)
-      return messageDispatch<Protocol>().withHandlers(repo, [
-        [Init, handleInit],
-        [SetOrigin, handleSetOrigin],
-        [Fetch, handleFetch],
-      ])
-    }
-
-    it('fetches the lastest commit from the origin remote', async () => {
-      const repoPath = path.resolve(root, 'a-repo-id')
-      const git = openRepo(repoPath)
-      await git(Init.bareRepo())
-      await git(SetOrigin.toUrl(originUrl))
-      await git(Fetch.fromOrigin())
-      const { stdout } = await exec('git rev-parse refs/remotes/origin/master', { cwd: repoPath })
-      assertThat(stdout, startsWith(latestCommit))
-    })
-
-    it('fetches only 1 commit from the origin remote', async () => {
-      const repoPath = path.resolve(root, 'a-repo-id')
-      const git = openRepo(repoPath)
-      await git(Init.bareRepo())
-      await git(SetOrigin.toUrl(originUrl))
-      await git(Fetch.fromOrigin())
-      const { stdout } = await exec('git rev-list --count refs/remotes/origin/master', {
-        cwd: repoPath,
-      })
-      assertThat(stdout.trim(), equalTo('1'))
-    })
-
-    it('fails when the remote does not exist', async () => {
-      const repoPath = path.resolve(root, 'a-repo-id')
-      const git = openRepo(repoPath)
-      await git(Init.bareRepo())
-      await git(SetOrigin.toUrl('invalid-remote-url'))
-      await promiseThat(
-        git(Fetch.fromOrigin()),
-        isRejectedWith(
-          hasProperty('message', matchesPattern('does not appear to be a git repository')),
-        ),
-      )
-    })
+  it('fails when the remote does not exist', async () => {
+    await git(SetOrigin.toUrl('invalid-remote-url'))
+    await promiseThat(
+      git(Fetch.fromOrigin()),
+      isRejectedWith(
+        hasProperty('message', matchesPattern('does not appear to be a git repository')),
+      ),
+    )
   })
 })
