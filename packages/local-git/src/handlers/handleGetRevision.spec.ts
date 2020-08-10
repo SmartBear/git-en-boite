75a861e1f41ac03b5ca742fbed0eaa886cd93393
Romain Gérard <romaingweb@gmail.com>
2020-08-10 11:21:29 +0200

    Remove non-bare repo from handleGetRevision
    Co-authored-by: Marju Hirsh <marjutubli@gmail.com>

--- packages/local-git/src/handlers/handleGetRevision.spec.ts
+++ packages/local-git/src/handlers/handleGetRevision.spec.ts
@@ -1,14 +1,14 @@
 import childProcess from 'child_process'
 import fs from 'fs'
-import { AsyncCommand, AsyncQuery, messageDispatch, Dispatch } from 'git-en-boite-message-dispatch'
-import { Commit, EnsureBranchExists, GetRevision, Init } from '../operations'
+import { AsyncCommand, AsyncQuery, Dispatch, messageDispatch } from 'git-en-boite-message-dispatch'
 import { equalTo, fulfilled, promiseThat, rejected } from 'hamjest'
 import path from 'path'
 import { dirSync } from 'tmp'
 import { promisify } from 'util'
 
 import { GitDirectory } from '../git_directory'
-import { handleCommitToNonBareRepo } from './handleCommitToNonBareRepo'
+import { Commit, EnsureBranchExists, GetRevision, Init } from '../operations'
+import { handleCommitToBareRepo } from './handleCommitToBareRepo'
 import { handleEnsureBranchExists } from './handleEnsureBranchExists'
 import { handleGetRevision } from './handleGetRevision'
 import { handleInit } from './handleInit'
@@ -22,58 +22,57 @@ type Protocol = [
   AsyncCommand<EnsureBranchExists>,
 ]
 
-describe('handleGetRevision', () => {
-  let root: string
+const repo = (repoPath: string) => {
+  fs.mkdirSync(repoPath, { recursive: true })
+  const repo = new GitDirectory(repoPath)
+  return messageDispatch<Protocol>().withHandlers(repo, [
+    [Init, handleInit],
+    [Commit, handleCommitToBareRepo],
+    [GetRevision, handleGetRevision],
+    [EnsureBranchExists, handleEnsureBranchExists],
+  ])
+}
 
-  beforeEach(() => (root = dirSync().name))
+const revisionForBranch = async (branchName: string, repoPath: string) => {
+  const result = await exec(`git rev-parse ${branchName}`, { cwd: repoPath })
+  return result.stdout.trim()
+}
+
+describe('handleGetRevision', () => {
+  const branchName = 'main'
+
+  let root: string
+  let git: Dispatch<Protocol>
+  let repoPath: string
+
+  beforeEach(async () => {
+    root = dirSync().name
+    repoPath = path.resolve(root, 'a-repo-id')
+    git = await repo(repoPath)
+    await git(Init.bareRepo())
+  })
+
   afterEach(function () {
     if (this.currentTest.state === 'failed' && this.currentTest.err)
       this.currentTest.err.message = `\nFailed using tmp directory:\n${root}\n${this.currentTest.err?.message}`
   })
 
-  const repo = (repoPath: string) => {
-    fs.mkdirSync(repoPath, { recursive: true })
-    const repo = new GitDirectory(repoPath)
-    return messageDispatch<Protocol>().withHandlers(repo, [
-      [Init, handleInit],
-      [Commit, handleCommitToNonBareRepo],
-      [GetRevision, handleGetRevision],
-      [EnsureBranchExists, handleEnsureBranchExists],
-    ])
-  }
+  context('with no commits in the repo', () => {
+    it('fails', async () => {
+      await promiseThat(git(GetRevision.forBranchNamed(branchName)), rejected())
+    })
+  })
 
-  const revisionForBranch = async (branchName: string, repoPath: string) => {
-    const result = await exec(`git rev-parse ${branchName}`, { cwd: repoPath })
-    return result.stdout.trim()
-  }
-
-  context('in a non-bare repo', () => {
-    let git: Dispatch<Protocol>
-    let repoPath: string
-
+  context('with a commit to the main branch', () => {
     beforeEach(async () => {
-      repoPath = path.resolve(root, 'a-repo-id')
-      git = await repo(repoPath)
-      await git(Init.nonBareRepo())
+      await git(Commit.withAnyMessage().toBranch(branchName))
     })
 
-    context('with no commits in the repo', () => {
-      it('fails', async () => {
-        await promiseThat(git(GetRevision.forBranchNamed('master')), rejected())
-      })
-    })
-
-    context('with a commit to the master branch', () => {
-      beforeEach(async () => {
-        await git(Commit.withAnyMessage())
-      })
-
-      it('returns the revision of the latest commit', async () => {
-        await promiseThat(
-          git(GetRevision.forBranchNamed('master')),
-          fulfilled(equalTo(await revisionForBranch('master', repoPath))),
-        )
-      })
+    it('returns the revision of the latest commit', async () => {
+      await promiseThat(
+        git(GetRevision.forBranchNamed(branchName)),
+        fulfilled(equalTo(await revisionForBranch(branchName, repoPath))),
+      )
     })
   })
 })
