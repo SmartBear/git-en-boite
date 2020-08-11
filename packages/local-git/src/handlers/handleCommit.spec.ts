64ff1961a88831a001039a5147a0f9204e73e22d
Marju Hirsh <marjutubli@gmail.com>
2020-08-11 12:34:02 +0300

    Start writing test for push
    Co-authored-by: Romain Gérard <romain.gerard@smartbear.com>

--- packages/local-git/src/handlers/handleCommit.spec.ts
+++ packages/local-git/src/handlers/handleCommit.spec.ts
@@ -6,25 +6,37 @@ import { dirSync } from 'tmp'
 
 import { handleCommit, handleInit } from '.'
 import { GitDirectory } from '../git_directory'
-import { Commit, Init } from '../operations'
+import { Commit, Init, SetOrigin, Fetch } from '../operations'
+import { handleSetOrigin } from './handleSetOrigin'
+import { handleFetch } from './handleFetch'
+import { BareRepoFactory } from '../bare_repo_factory'
 
-type Protocol = [AsyncCommand<Commit>, AsyncCommand<Init>]
+type Protocol = [
+  AsyncCommand<Commit>,
+  AsyncCommand<Init>,
+  AsyncCommand<Fetch>,
+  AsyncCommand<SetOrigin>,
+]
 
-describe('handleCommit', () => {
+describe('@wip handleCommit', () => {
   const branchName = 'a-branch'
   let root: string
   let repoPath: string
+  let originUrl: string
   let git: Dispatch<Protocol>
   let repo: GitDirectory
 
   beforeEach(async () => {
     root = dirSync().name
     repoPath = path.resolve(root, 'a-repo-id')
+    originUrl = path.resolve(root, 'remote', 'a-repo-id')
     fs.mkdirSync(repoPath, { recursive: true })
     repo = new GitDirectory(repoPath)
     git = messageDispatch<Protocol>().withHandlers(repo, [
       [Commit, handleCommit],
       [Init, handleInit],
+      [Fetch, handleFetch],
+      [SetOrigin, handleSetOrigin],
     ])
     await git(Init.bareRepo())
   })
@@ -35,9 +47,9 @@ describe('handleCommit', () => {
   })
 
   it('creates an empty commit with the given message', async () => {
-    await git(Commit.withMessage('A commit message'))
+    await git(Commit.withMessage('A commit message').toBranch(branchName))
     await promiseThat(
-      repo.execGit('log', ['main', '--oneline']),
+      repo.execGit('log', [`refs/drafts/${branchName}`, '--oneline']),
       fulfilled(hasProperty('stdout', containsString('A commit message'))),
     )
   })
@@ -46,7 +58,7 @@ describe('handleCommit', () => {
     const file = { path: 'a.file', content: 'some content' }
     await git(Commit.newFile(file).toBranch(branchName))
     await promiseThat(
-      repo.execGit('ls-tree', [branchName, '-r', '--name-only']),
+      repo.execGit('ls-tree', [`refs/drafts/${branchName}`, '-r', '--name-only']),
       fulfilled(hasProperty('stdout', containsString(file.path))),
     )
   })
@@ -55,7 +67,7 @@ describe('handleCommit', () => {
     await git(Commit.withMessage('initial commit'))
     await git(Commit.withMessage('A commit message'))
     await promiseThat(
-      repo.execGit('log', ['main', '--oneline']),
+      repo.execGit('log', [`refs/drafts/${branchName}`, '--oneline']),
       fulfilled(hasProperty('stdout', containsStrings('initial commit', 'A commit message'))),
     )
   })
@@ -65,8 +77,19 @@ describe('handleCommit', () => {
     const file = { path: 'a.file', content: 'some content' }
     await git(Commit.newFile(file).toBranch(branchName))
     await promiseThat(
-      repo.execGit('ls-tree', [branchName, '-r', '--name-only']),
+      repo.execGit('ls-tree', [`refs/drafts/${branchName}`, '-r', '--name-only']),
       fulfilled(hasProperty('stdout', not(containsString('junk.file')))),
     )
+  })
+
+  it.only('pushes to remote', async () => {
+    const origin = await new BareRepoFactory().open(originUrl)
+    await origin(Commit.withAnyMessage().toBranch(branchName))
+    await git(SetOrigin.toUrl(originUrl))
+    await repo.addFileToIndex({ path: 'junk.file', content: 'Junk' })
+    const file = { path: 'a.file', content: 'some content' }
+    await git(Commit.newFile(file).toBranch(branchName))
+    await git(Fetch.fromOrigin())
+    const { stdout } = await repo.execGit('show-refs', ['--heads'])
   })
 })
