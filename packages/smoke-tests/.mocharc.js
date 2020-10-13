b64d4e9e476fc2759667bfc1a011c5a96f82159f
cbohiptest <celine.bon@hiptest.net>
2020-10-13 14:26:20 +0200

    Fix mocha settings Co-authored-by: Julien Biezemans <jb@jbpros.com>

--- packages/smoke-tests/.mocharc.js
+++ packages/smoke-tests/.mocharc.js
@@ -1,7 +1,7 @@
 process.env['NODE_ENV'] = 'test'
 module.exports = {
   "extension": ["ts"],
-  "spec": "src/**/*.spec.ts",
+  "spec": "index.ts",
   "require": "ts-node/register",
   "color": true
 }
