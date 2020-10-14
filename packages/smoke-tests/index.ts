c44548870eeff77318764cd5ce46653724b76e81
Julien Biezemans <jb@jbpros.com>
2020-10-14 10:41:33 +0200

    Refactor

--- packages/smoke-tests/index.ts
+++ packages/smoke-tests/index.ts
@@ -1,7 +1,4 @@
-import { assertThat, equalTo } from 'hamjest'
-import fetch from 'node-fetch'
-import { v4 as uuid } from 'uuid'
-import { default as supertest} from 'supertest'
+import fs from "fs"
 import {
   Author,
   BranchName,
@@ -10,26 +7,31 @@ import {
   Files,
   NameOfPerson,
   RepoId,
-} from 'git-en-boite-core'
-import { GitDirectory } from 'git-en-boite-local-clones'
-import fs from 'fs'
-import os from 'os'
-import path from 'path'
+} from "git-en-boite-core"
+import { GitDirectory } from "git-en-boite-local-clones"
+import { assertThat, equalTo } from "hamjest"
+import fetch from "node-fetch"
+import os from "os"
+import path from "path"
+import supertest from "supertest"
+import { v4 as uuid } from "uuid"
 
 const url = process.env.smoke_tests_web_server_url
 const remoteUrl = process.env.smoke_tests_remote_repo_url
 
-if(!url) throw new Error("Please define smoke_tests_web_server_url env var")
-if(!remoteUrl) throw new Error("Please define smoke_tests_remote_repo_url env var")
+if (!url) throw new Error("Please define smoke_tests_web_server_url env var")
+if (!remoteUrl)
+  throw new Error("Please define smoke_tests_remote_repo_url env var")
 
-describe('smoke test', function(){
+describe("smoke test", function () {
   this.timeout(10000)
 
   const repoId = RepoId.of(`smoke-test-${uuid()}`)
   const localRepoPath = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`)
   const localRepo = new GitDirectory(localRepoPath)
+  const branchName = BranchName.of("smoke-tests")
 
-  before(async ()=>{
+  before(async () => {
     await localRepo.exec("init")
     await localRepo.exec("remote", ["add", "origin", remoteUrl])
     await cleanUpRemoteRepo()
@@ -37,9 +39,20 @@ describe('smoke test', function(){
 
   after(cleanUpRemoteRepo)
 
-  async function cleanUpRemoteRepo(){
-    if ((await localRepo.exec("ls-remote", ["origin", "refs/heads/nouvelle-branche"])).stdout !== "")
-      await localRepo.exec("push", ["origin", "--delete", "nouvelle-branche"])
+  async function cleanUpRemoteRepo() {
+    if (
+      (
+        await localRepo.exec("ls-remote", [
+          "origin",
+          `refs/heads/${branchName}`,
+        ])
+      ).stdout !== ""
+    )
+      await localRepo.exec("push", [
+        "origin",
+        "--delete",
+        branchName.toString(),
+      ])
   }
 
   it(`Checks if the server is up: ${url}`, async () => {
@@ -47,33 +60,35 @@ describe('smoke test', function(){
     assertThat(response.status, equalTo(200))
   })
 
-  it('Creates a repo', async() => {
-    const params = {'repoId': repoId, 'remoteUrl': remoteUrl};
-    const response = await fetch(`${url}/repos`,
-      {
-        method: 'POST',
-        body: JSON.stringify(params),
-        headers: {'Content-Type':'application/json'}
-      }
-    )
+  it("Creates a repo", async () => {
+    const params = { repoId: repoId, remoteUrl: remoteUrl }
+    const response = await fetch(`${url}/repos`, {
+      method: "POST",
+      body: JSON.stringify(params),
+      headers: { "Content-Type": "application/json" },
+    })
     assertThat(response.status, equalTo(202))
   })
 
-  it('Waits for repo to be fetched', async() => {
-    const response = await fetch(`${url}/repos/${repoId}/events?until=repo.fetched`)
+  it("Waits for repo to be fetched", async () => {
+    const response = await fetch(
+      `${url}/repos/${repoId}/events?until=repo.fetched`
+    )
     assertThat(response.status, equalTo(200))
   })
 
-  it('Gets repo branches details', async() => {
+  it("Gets repo branches details", async () => {
     const response = await fetch(`${url}/repos/${repoId}`)
     assertThat((await response.json()).branches, equalTo([]))
   })
 
-  it('Commits a file', async() => {
-    const branchName = BranchName.of('nouvelle-branche')
-    const files = Files.fromJSON([{ path: 'path', content: 'content' }])
-    const author = new Author(new NameOfPerson('Bob'), new Email('bob@example.com'))
-    const message = CommitName.of('a message')
+  it("Commits a file", async () => {
+    const files = Files.fromJSON([{ path: "path", content: "content" }])
+    const author = new Author(
+      new NameOfPerson("Bob"),
+      new Email("bob@example.com")
+    )
+    const message = CommitName.of("a message")
 
     let request = supertest(url)
 
