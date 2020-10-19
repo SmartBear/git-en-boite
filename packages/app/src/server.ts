502c7660055fa8cb8fc07606462f5923903f9d79
Julien Biezemans <jb@jbpros.com>
2020-10-19 16:23:45 +0200

    Extract smoke test function
    Co-authored-by: Matt Wynne <matt@mattwynne.net>

--- packages/app/src/server.ts
+++ packages/app/src/server.ts
@@ -1,36 +1,16 @@
-import { spawn } from 'child_process'
 import { EventEmitter } from 'events'
 import { createConfig } from 'git-en-boite-config'
 import { Application, DomainEventBus, fetchRepoAfterConnected } from 'git-en-boite-core'
 import { BackgroundWorkerLocalClones, DirectLocalClone } from 'git-en-boite-local-clones'
 import { DiskRepoIndex } from 'git-en-boite-repo-index'
 import { startWebServer } from 'git-en-boite-web'
-import path from 'path'
+import { runSmokeTests } from 'git-en-boite-smoke-tests'
 
 import { LaBoîte } from './la_boîte'
 
 const config = createConfig(process.env)
 console.log(`git-en-boite starting up...`)
 console.log(`Using config: ${JSON.stringify(config, null, 2)}`)
-
-const runSmokeTests = async (url: string) => {
-  const cwd = path.resolve(__dirname, '../..')
-  const child = spawn('yarn', ['smoke', 'start'], {
-    cwd,
-    stdio: 'inherit',
-    env: {
-      ...process.env,
-      smoke_tests_web_server_url: url,
-    },
-  })
-
-  return new Promise((resolve, reject) => {
-    child.on('exit', status => {
-      if (status !== 0) return reject(new Error('Smoke tests failed'))
-      resolve()
-    })
-  })
-}
 
 inConsole(async () => {
   const domainEvents: DomainEventBus = new EventEmitter()
@@ -47,13 +27,10 @@ inConsole(async () => {
   )
 
   const port = 3001
+  const url = `http://localhost:${port}`
   startWebServer(app, port)
-  if (process.env.smoke_tests_remote_repo_url) {
-    console.log('Running smoke tests...')
-    await runSmokeTests('http://localhost:3001')
-    console.log('Done...')
-  }
-  console.log(`Server listening on http://localhost:${port}`)
+  await runSmokeTests(url)
+  console.log(`Server listening on ${url}`)
 })
 
 function inConsole(start: () => Promise<void>): void {
--- packages/smoke-tests/index.ts
+++ packages/smoke-tests/index.ts
@@ -1,3 +1,4 @@
+import { spawn } from 'child_process'
 import fs from 'fs'
 import {
   Author,
@@ -80,3 +81,23 @@ describe('smoke test', function () {
       .expect(200)
   })
 })
+
+export const runSmokeTests = async (url: string): Promise<void> => {
+  if (!process.env.smoke_tests_remote_repo_url) return
+  const cwd = path.resolve(__dirname)
+  const child = spawn('yarn', ['start'], {
+    cwd,
+    stdio: 'inherit',
+    env: {
+      ...process.env,
+      smoke_tests_web_server_url: url,
+    },
+  })
+
+  return new Promise((resolve, reject) => {
+    child.on('exit', status => {
+      if (status !== 0) return reject(new Error('Smoke tests failed'))
+      resolve()
+    })
+  })
+}
