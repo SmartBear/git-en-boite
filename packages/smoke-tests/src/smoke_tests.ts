6b900353e6c96c9a6f322db8ce2b6fcdbfa8678b
Matt Wynne <matt@mattwynne.net>
2020-10-19 16:40:07 +0100

    Tidy away smoke tests into `src` folder

--- packages/smoke-tests/src/smoke_tests.ts
+++ packages/smoke-tests/src/smoke_tests.ts
@@ -81,23 +81,3 @@ describe('smoke test', function () {
       .expect(200)
   })
 })
-
-export const runSmokeTests = async (url: string): Promise<void> => {
-  if (!process.env.smoke_tests_remote_repo_url) return
-  const cwd = path.resolve(__dirname)
-  const child = spawn('yarn', ['start'], {
-    cwd,
-    stdio: 'inherit',
-    env: {
-      ...process.env,
-      smoke_tests_web_server_url: url,
-    },
-  })
-
-  return new Promise((resolve, reject) => {
-    child.on('exit', status => {
-      if (status !== 0) return reject(new Error('Smoke tests failed'))
-      resolve()
-    })
-  })
-}
