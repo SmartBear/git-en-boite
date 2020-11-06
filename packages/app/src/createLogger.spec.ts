6117ec3b914535a2675184f88ffe7a667060ebaa
Matt Wynne <matt@mattwynne.net>
2020-11-06 12:27:41 +0000

    Refactor and extend log sanitizing
    Relates-to: #287

--- packages/app/src/createLogger.spec.ts
+++ packages/app/src/createLogger.spec.ts
@@ -1,22 +1,46 @@
+import { assertThat, containsString } from 'hamjest'
+
 import { createLogger } from './createLogger'
-import { assertThat, containsString, equalTo } from 'hamjest'
 
-describe(createLogger.name, () => {
-  const logger = createLogger({ readableBy: 'human' })
+describe(createLogger.name + '@wip', () => {
+  context('in human-readable mode', () => {
+    const logger = createLogger({ readableBy: 'human' })
 
-  it('logs to the console', () => {
-    const lines = captureStdOut(() => {
-      logger.info('test', { one: { two: 3 } })
-    })
-    assertThat(lines[0], containsString('test'))
-  })
-
-  describe('removing sensitive fields', () => {
-    it('sanitizes remoteUrl in the root of the metadata', () => {
+    it('logs to the console', () => {
       const lines = captureStdOut(() => {
-        logger.info('test', { remoteUrl: 'https://token@github.com/org/project' })
+        logger.info('this is a test')
       })
-      assertThat(lines[0], containsString('https://***@github.com/org/project'))
+      assertThat(lines[0], containsString('this is a test'))
+    })
+
+    it('logs arbitrary data as JSON', () => {
+      const lines = captureStdOut(() => {
+        logger.info('test', { data: { one: { two: 3 } } })
+      })
+      assertThat(lines[0], containsString('{"one":{"two":3}}'))
+    })
+
+    describe('removing sensitive fields', () => {
+      it('sanitizes remoteUrl in the root of the metadata', () => {
+        const lines = captureStdOut(() => {
+          logger.info('test', { remoteUrl: 'https://token@github.com/org/project' })
+        })
+        assertThat(lines[0], containsString('https://***@github.com/org/project'))
+      })
+
+      it('sanitizes remoteUrl deep in the metadata', () => {
+        const lines = captureStdOut(() => {
+          logger.info('test', { data: { remoteUrl: 'https://token@github.com/org/project' } })
+        })
+        assertThat(lines[0], containsString('https://***@github.com/org/project'))
+      })
+
+      it('sanitizes token deep in the metadata', () => {
+        const lines = captureStdOut(() => {
+          logger.info('test', { data: { token: 'a-token' } })
+        })
+        assertThat(lines[0], containsString('"token":"***"'))
+      })
     })
   })
 })
--- packages/app/src/createLogger.ts
+++ packages/app/src/createLogger.ts
@@ -2,24 +2,76 @@ import { LoggerOptions } from 'git-en-boite-config'
 import { Logger } from 'git-en-boite-core'
 import * as winston from 'winston'
 
-const sanitize = () => ({
+const sanitize = (...definitions: SantizedFieldDefinition[]) => ({
   transform: (info: winston.Logform.TransformableInfo) => {
-    for (const field of Object.keys(info)) {
-      console.error(field, JSON.stringify(info[field]))
-      if (field == 'remoteUrl') {
-        info.remoteUrl = info.remoteUrl.replace(/(https:\/\/)(\w+)(@.+)/, '$1***$3')
-      }
+    const sanitizers = definitions
+      .map(definition => new SanitizeField(definition))
+      .reduce((sanitizers: Record<string, SanitizeField>, sanitizer) => {
+        sanitizers[sanitizer.field] = sanitizer
+        return sanitizers
+      }, {})
+
+    const recurse = {
+      apply: (object: any, field: string) => {
+        object[field] = sanitizeObject(object[field])
+      },
     }
-    return info
+    return sanitizeObject(info)
+
+    function sanitizeObject(object: any) {
+      if (typeof object !== 'object') return object
+      for (const field of Object.keys(object)) {
+        const sanitizer = sanitizers[field] || recurse
+        sanitizer.apply(object, field)
+      }
+      return object
+    }
   },
 })
+
+type SantizedFieldDefinition = {
+  field: string
+  pattern?: RegExp
+  replacement?: string
+}
+
+class SanitizeField {
+  public readonly field
+  constructor(private readonly definition: SantizedFieldDefinition) {
+    this.field = definition.field
+    this.definition.pattern = this.definition.pattern || /.*/
+    this.definition.replacement = this.definition.replacement || '***'
+  }
+
+  appliesTo(field: string) {
+    return field === this.definition.field
+  }
+
+  apply(object: any, field: string): boolean {
+    if (!this.appliesTo(field)) return
+    if (typeof object[field] !== 'string') throw new Error('Unable to sanitize field')
+    object[field] = object[field].replace(this.definition.pattern, this.definition.replacement)
+    return true
+  }
+}
 
 const transports = [new winston.transports.Console()]
 
 const loggers = {
   human: winston.createLogger({
     level: 'info',
-    format: winston.format.combine(sanitize(), winston.format.colorize(), winston.format.simple()),
+    format: winston.format.combine(
+      sanitize(
+        {
+          field: 'remoteUrl',
+          pattern: /(https:\/\/)(\w+)(@.+)/,
+          replacement: '$1***$3',
+        },
+        { field: 'token' },
+      ),
+      winston.format.prettyPrint(),
+      winston.format.simple(),
+    ),
     transports,
   }),
   machine: winston.createLogger({
