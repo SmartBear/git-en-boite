bff3d349074aa477ecab9c11b43933f964125b57
Matt Wynne <matt@mattwynne.net>
2020-11-11 10:18:41 +0000

    Silence Koa's default stderr error logging
    Added some characterisation tests around the createWebApp function to
    pin down how the error handling / logging behaviour of the app works.
    
    Cabin is still a bit of a mystery, and how it behaves in concert with
    Koa's default behaviour and any other middleware we write is useful to
    characterise.
    
    Next: figure out what Cabin already logs about exceptions, and decide
    whether to:
    
    - extend what Cabin logs in the http request log to include all error
      info.
    - add a second log message about the uncaught exception.
    
    Relates-to: #287

---
+++ packages/web/src/create_web_app.spec.ts
@@ -0,0 +1,121 @@
+import Router from '@koa/router'
+import { Logger } from 'git-en-boite-core'
+import { assertThat, equalTo, hasProperty, isEmpty, matchesPattern } from 'hamjest'
+import { wasCalled, wasCalledWith } from 'hamjest-sinon'
+import { Context } from 'koa'
+import supertest, { SuperTest, Test } from 'supertest'
+import { stubInterface } from 'ts-sinon'
+
+import createWebApp from './create_web_app'
+
+describe(createWebApp.name + 'createWebApp @wip', () => {
+  describe('routing requests', () => {
+    it('handles a regular route', async () => {
+      const logger = stubInterface<Logger>()
+      const app = createWebApp(
+        new Router().get('/', (ctx: Context) => (ctx.status = 200)),
+        logger,
+      )
+      const server = app.listen()
+      const request = supertest(server)
+      await request.get('/').expect(200)
+      server.close()
+    })
+  })
+
+  describe('logging requests', () => {
+    it('logs 200 responses as info', async () => {
+      const logger = stubInterface<Logger>()
+      const app = createWebApp(
+        new Router().get('/', (ctx: Context) => (ctx.status = 200)),
+        logger,
+      )
+      const server = app.listen()
+      const request = supertest(server)
+      await request.get('/').expect(200)
+      assertThat(logger.info, wasCalled())
+      assertThat(
+        logger.info,
+        wasCalledWith(
+          matchesPattern(/GET/),
+          hasProperty('request', hasProperty('url', equalTo('/'))),
+        ),
+      )
+      server.close()
+    })
+
+    it('logs 400 responses as warn', async () => {
+      const logger = stubInterface<Logger>()
+      const app = createWebApp(
+        new Router().get('/', async (ctx: Context) => {
+          ctx.throw(400)
+        }),
+        logger,
+      )
+      const server = app.listen()
+      const request = supertest(server)
+      await request.get('/').expect(400)
+      assertThat(logger.warn, wasCalled())
+      assertThat(
+        logger.warn,
+        wasCalledWith(
+          matchesPattern(/GET/),
+          hasProperty('request', hasProperty('url', equalTo('/'))),
+        ),
+      )
+      server.close()
+    })
+
+    it('logs 500 responses as error', async () => {
+      const logger = stubInterface<Logger>()
+      const app = createWebApp(
+        new Router().get('/', (ctx: Context) => ctx.throw(new Error('yikes'))),
+        logger,
+      )
+      const server = app.listen()
+      const request = supertest(server)
+      await request.get('/').expect(500)
+      assertThat(logger.error, wasCalled())
+      assertThat(
+        logger.error,
+        wasCalledWith(
+          matchesPattern(/GET/),
+          hasProperty('request', hasProperty('url', equalTo('/'))),
+        ),
+      )
+      server.close()
+    })
+  })
+
+  describe('handling exceptions', () => {
+    it('does not write anything to stderr', async () => {
+      const logger = stubInterface<Logger>()
+      const error = new Error('yikes')
+      const app = createWebApp(
+        new Router().get('/', (ctx: Context) => ctx.throw(error)),
+        logger,
+      )
+      const server = app.listen()
+      const request = supertest(server)
+      const lines = await captureStdErr(async () => {
+        await request.get('/').expect(500)
+      })
+      assertThat(lines, isEmpty())
+      server.close()
+
+      async function captureStdErr(fn: () => Promise<void>) {
+        const lines: string[] = []
+        const original = process.stderr.write
+        process.stderr.write = (line: string) => !!lines.push(line)
+        try {
+          await fn()
+          return lines
+        } catch (error) {
+          throw error
+        } finally {
+          process.stderr.write = original
+        }
+      }
+    })
+  })
+})
--- packages/web/src/create_web_app.ts
+++ packages/web/src/create_web_app.ts
@@ -1,20 +1,22 @@
 import Router from '@koa/router'
 import Cabin from 'cabin'
 import { Logger } from 'git-en-boite-core'
-import Koa from 'koa'
+import Koa, { Context, Next } from 'koa'
 import bodyParser from 'koa-bodyparser'
 import cors from 'koa2-cors'
 
 export default (routes: Router = new Router(), logger: Logger): Koa => {
   const webApp = new Koa()
-  webApp.use(bodyParser())
-  webApp.use(cors({ origin: '*' }))
+  webApp.silent = true
+  // webApp.use((ctx: Context, next: Next) => next().catch(logger.error))
   webApp.use(
     new Cabin({
       axe: { logger },
       capture: false,
     }).middleware,
   )
+  webApp.use(bodyParser())
+  webApp.use(cors({ origin: '*' }))
   webApp.use(routes.middleware())
   webApp.use(routes.allowedMethods())
   return webApp
