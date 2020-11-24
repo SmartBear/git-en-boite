c27f600e9abed5cac03ac4354fd7999a28f090d3
Matt Wynne <matt@mattwynne.net>
2020-11-24 17:55:47 +0000

    Fix config specs
    Relates-to: #319

--- packages/config/src/config.spec.ts
+++ packages/config/src/config.spec.ts
@@ -1,4 +1,4 @@
-import { assertThat, equalTo, hasProperty, matchesPattern, throws } from 'hamjest'
+import { assertThat, containsString, equalTo, hasProperty, matchesPattern, throws } from 'hamjest'
 
 import { createConfig } from './config'
 
@@ -7,12 +7,16 @@ describe('createConfig', () => {
     GIT_ROOT: 'some/git-root',
     NODE_ENV: 'any',
     REDIS_URL: 'redis://someredis',
+    npm_package_version: '1.2.3',
+    build_number: '456',
+    git_ref: 'abc',
   }
 
   it('throws an error if NODE_ENV is not set', () => {
+    const badEnv = Object.assign({}, defaultEnv, { NODE_ENV: undefined })
     assertThat(
-      () => createConfig({}),
-      throws(hasProperty('message', equalTo('Please set NODE_ENV'))),
+      () => createConfig(badEnv),
+      throws(hasProperty('message', containsString('"NODE_ENV" is a required variable'))),
     )
   })
 
@@ -35,32 +39,9 @@ describe('createConfig', () => {
   })
 
   context('version', () => {
-    it('raises an error if the .build-number file cannot be found', () => {
-      const fakeFs = {
-        existsSync: (path: string) => {
-          if (path.match(/\.build-number$/)) return false
-          throw new Error(`path ${path} not faked`)
-        },
-      }
-      assertThat(
-        () => createConfig({ ...defaultEnv, npm_package_version: '1.2.3' }, fakeFs),
-        throws(hasProperty('message', matchesPattern('Build number file not found'))),
-      )
-    })
-
-    it('returns the version from package.json with appended build number if a .build-number file exists', () => {
-      const fakeFs = {
-        readFileSync: (path: string) => {
-          if (path.match(/\.build-number$/)) return '456'
-          throw new Error(`path ${path} not faked`)
-        },
-        existsSync: (path: string) => {
-          if (path.match(/\.build-number$/)) return true
-          throw new Error(`path ${path} not faked`)
-        },
-      }
-      const config = createConfig({ ...defaultEnv, npm_package_version: '1.2.3' }, fakeFs)
-      assertThat(config, hasProperty('version', equalTo('1.2.3.456')))
+    it('returns the version from package.json', () => {
+      const config = createConfig(defaultEnv)
+      assertThat(config, hasProperty('version', equalTo('1.2.3.456#abc')))
     })
   })
 })
