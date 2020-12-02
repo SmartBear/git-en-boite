7280ba5a939381b88a5e5b206e4228e9048195f0
16sheep <marjutubli@gmail.com>
2020-12-02 19:14:15 +0200

    Make repo creation accept a transaction #342 (#364)
    Co-authored-by: Matt Wynne <matt@mattwynne.net>
    Co-authored-by: Julien Biezemans <jb@jbpros.com>

--- packages/inventory/.eslintrc.js
+++ packages/inventory/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     sourceType: 'module', // Allows for the use of imports
   },
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 0
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-empty-function': ['off'],
   },
 }
