e8efe7adee41c75a1f9dd894af061c46895dd0a8
Matt Wynne <matt@mattwynne.net>
2021-03-05 10:20:19 -0800

    Log domain events

--- packages/app/src/server.ts
+++ packages/app/src/server.ts
@@ -1,15 +1,28 @@
 import { EventEmitter } from 'events'
 import { Config } from 'git-en-boite-config'
-import { Application, DomainEventBus, fetchRepoAfterConnected, WriteLogEvent } from 'git-en-boite-core'
+import {
+  Application,
+  DomainEventBus,
+  DomainEvents,
+  DomainRule,
+  fetchRepoAfterConnected,
+  WriteLogEvent,
+} from 'git-en-boite-core'
 import { InventoryOfReposOnDisk } from 'git-en-boite-inventory'
 import { BackgroundWorkerLocalClones, DirectLocalClones } from 'git-en-boite-local-clones'
 import { startWebServer } from 'git-en-boite-web'
+import { info } from 'winston'
 
 import { LaBoîte } from './la_boîte'
 import { runProcess } from './runProcess'
 
 runProcess(async (config: Config, log: WriteLogEvent) => {
   log({ level: 'info', message: `git-en-boite server starting up`, config })
+  const logDomainEvents: DomainRule = (events) => {
+    for (const eventKey of DomainEvents.keys) {
+      events.on(eventKey, (event) => log({ level: 'info', message: eventKey, event }))
+    }
+  }
   const domainEvents: DomainEventBus = new EventEmitter()
   const localClones = await BackgroundWorkerLocalClones.connect(
     new DirectLocalClones(),
@@ -19,7 +32,13 @@ runProcess(async (config: Config, log: WriteLogEvent) => {
   )
   await localClones.pingWorkers()
   const inventoryOfRepos = new InventoryOfReposOnDisk(config.git.root, localClones, domainEvents)
-  const app: Application = new LaBoîte(inventoryOfRepos, config.version, domainEvents, [fetchRepoAfterConnected], log)
+  const app: Application = new LaBoîte(
+    inventoryOfRepos,
+    config.version,
+    domainEvents,
+    [logDomainEvents, fetchRepoAfterConnected],
+    log
+  )
 
   const port = 3001
   startWebServer(app, port, log)
