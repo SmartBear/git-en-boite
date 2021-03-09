85b37d03bdfdb05862f884e8c2b310f8720e9d93
Matt Wynne <matt@mattwynne.net>
2021-03-08 21:29:59 -0800

    Build a GlobalEventBus
    Ref #242

---
+++ packages/app/src/GlobalEventBus.spec.ts
@@ -0,0 +1,62 @@
+import { createConfig } from 'git-en-boite-config'
+import { RepoConnected, RepoFetched, RepoId } from 'git-en-boite-core'
+import { assertThat, equalTo } from 'hamjest'
+
+import { GlobalEventBus } from './GlobalEventBus'
+
+const config = createConfig()
+
+describe(GlobalEventBus.name, () => {
+  it('transmits an event to a single receiver', async () => {
+    const sender = await GlobalEventBus.connect(config.redis)
+    const receiver = await GlobalEventBus.connect(config.redis)
+    const receiving = new Promise((resolve) => receiver.on('repo.connected', resolve))
+    const event = new RepoConnected(RepoId.generate())
+    sender.emit('repo.connected', event)
+    const actual = await receiving
+    assertThat(actual, equalTo(event))
+  })
+
+  it('transmits the same event to a multiple listeners on the same receiver', async () => {
+    const sender = await GlobalEventBus.connect(config.redis)
+    const receiver = await GlobalEventBus.connect(config.redis)
+    const receivingOne = new Promise((resolve) => receiver.on('repo.connected', resolve))
+    const receivingTwo = new Promise((resolve) => receiver.on('repo.connected', resolve))
+    const event = new RepoConnected(RepoId.generate())
+    sender.emit('repo.connected', event)
+    const actualOne = await receivingOne
+    const actualTwo = await receivingTwo
+    assertThat(actualOne, equalTo(event))
+    assertThat(actualTwo, equalTo(event))
+  })
+
+  it('transmits the same event to a multiple receivers', async () => {
+    const sender = await GlobalEventBus.connect(config.redis)
+    const receiverOne = await GlobalEventBus.connect(config.redis)
+    const receiverTwo = await GlobalEventBus.connect(config.redis)
+    const receivingOne = new Promise((resolve) => receiverOne.on('repo.connected', resolve))
+    const receivingTwo = new Promise((resolve) => receiverTwo.on('repo.connected', resolve))
+    const event = new RepoConnected(RepoId.generate())
+    sender.emit('repo.connected', event)
+    const actualOne = await receivingOne
+    const actualTwo = await receivingTwo
+    assertThat(actualOne, equalTo(event))
+    assertThat(actualTwo, equalTo(event))
+  })
+
+  it('transmits different events to different receivers', async () => {
+    const sender = await GlobalEventBus.connect(config.redis)
+    const receiverOne = await GlobalEventBus.connect(config.redis)
+    const receiverTwo = await GlobalEventBus.connect(config.redis)
+    const receivingOne = new Promise((resolve) => receiverOne.on('repo.connected', resolve))
+    const receivingTwo = new Promise((resolve) => receiverTwo.on('repo.fetched', resolve))
+    const repoConnected = new RepoConnected(RepoId.generate())
+    const repoFetched = new RepoFetched(RepoId.generate())
+    sender.emit('repo.connected', repoConnected)
+    sender.emit('repo.fetched', repoFetched)
+    const actualOne = await receivingOne
+    const actualTwo = await receivingTwo
+    assertThat(actualOne, equalTo(repoConnected))
+    assertThat(actualTwo, equalTo(repoFetched))
+  })
+})
---
+++ packages/app/src/GlobalEventBus.ts
@@ -0,0 +1,44 @@
+import { DomainEventBus, DomainEvents, fromJSON } from 'git-en-boite-core'
+import IORedis, { Redis } from 'ioredis'
+import EventEmitter from 'events'
+
+export class GlobalEventBus implements DomainEventBus {
+  static async connect(config: string): Promise<GlobalEventBus> {
+    const sub = await connectToRedis(config)
+    const listeners = new EventEmitter()
+    await new Promise<void>((resolve) => {
+      sub.subscribe(...DomainEvents.keys, () => {
+        sub.on('message', (channel, message) => {
+          listeners.emit(channel, fromJSON(JSON.parse(message)))
+        })
+        resolve()
+      })
+    })
+    const pub = await connectToRedis(config)
+    return new GlobalEventBus(pub, listeners)
+  }
+
+  constructor(private readonly pub: Redis, private readonly listeners: EventEmitter) {}
+
+  emit<Key extends keyof DomainEvents>(eventName: Key, event: DomainEvents[Key]): void {
+    this.pub.publish(eventName, JSON.stringify(event.toJSON()))
+  }
+  on<Key extends keyof DomainEvents>(eventName: Key, fn: (params: DomainEvents[Key]) => void): void {
+    this.listeners.on(eventName, fn)
+  }
+  off<Key extends keyof DomainEvents>(eventName: Key, fn: (params: DomainEvents[Key]) => void): void {
+    this.listeners.off(eventName, fn)
+  }
+}
+
+async function connectToRedis(url: string): Promise<Redis> {
+  const connection = new IORedis(url)
+  return new Promise((resolve, reject) =>
+    connection
+      .on('connect', () => resolve(connection))
+      .on('error', (error) => {
+        connection.disconnect()
+        reject(new Error(`Unable to connect to Redis: ${error.message}`))
+      })
+  )
+}
