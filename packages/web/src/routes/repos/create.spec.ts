57aaa63671a0cc96b8cfd7b8617fc014dc5c917c
Matt Wynne <matt@mattwynne.net>
2021-03-10 14:55:25 -0800

    Tidy up POST /repos endpoint
    Ref #334

--- packages/web/src/routes/repos/create.spec.ts
+++ packages/web/src/routes/repos/create.spec.ts
@@ -1,22 +1,14 @@
-import {
-  AccessDenied,
-  Application,
-  InvalidRepoUrl,
-  QueryResult,
-  RemoteUrl,
-  RepoId,
-  RepoSnapshot,
-} from 'git-en-boite-core'
-import { assertThat, equalTo } from 'hamjest'
+import { AccessDenied, Application, InvalidRepoUrl, QueryResult, RemoteUrl, RepoId } from 'git-en-boite-core'
+import { assertThat, equalTo, matchesPattern } from 'hamjest'
 import { wasCalled } from 'hamjest-sinon'
 import { Server } from 'http'
+import LinkHeader from 'http-link-header'
+import sinon from 'sinon'
 import supertest, { SuperTest, Test } from 'supertest'
 import { StubbedInstance, stubInterface } from 'ts-sinon'
 
 import createWebApp from '../../create_web_app'
 import router from '../router'
-
-const bareObject = <T>(object: T): Record<string, unknown> => JSON.parse(JSON.stringify(object))
 
 describe('POST /repos', () => {
   let request: SuperTest<Test>
@@ -38,48 +30,38 @@ describe('POST /repos', () => {
   })
 
   it('connects to the remote repo', async () => {
-    const repoId = RepoId.of('a-repo-id')
     const remoteUrl = RemoteUrl.of('../tmp')
     app.getInfo.resolves(QueryResult.from())
-    app.connectToRemote.withArgs(repoId, remoteUrl).resolves()
-    await request.post('/repos').send({ repoId: repoId.value, remoteUrl }).expect(202)
+    app.connectToRemote.withArgs(sinon.match.instanceOf(RepoId), remoteUrl).resolves()
+    await request.post('/repos').send({ remoteUrl }).expect(201)
     assertThat(app.connectToRemote, wasCalled())
   })
 
-  it('redirects to the repo if it already exists', async () => {
-    const repoId = RepoId.of('a-repo-id/with-slashes')
-    const repoInfo = new RepoSnapshot(repoId, [])
-    app.getInfo.resolves(QueryResult.from(repoInfo))
-    const connectRepoRequest = { repoId, remoteUrl: '../tmp' }
-    await request.post('/repos').send(connectRepoRequest).expect(302)
-    const response = await request.post('/repos').send(connectRepoRequest).redirects(1)
-    assertThat(response.body, equalTo(bareObject(repoInfo)))
+  it('puts a link to the repo in the header', async () => {
+    const connectRepoRequest = { remoteUrl: '../tmp' }
+    const createResponse = await request.post('/repos').send(connectRepoRequest)
+    const uri = LinkHeader.parse(createResponse.headers.link).rel('item')[0].uri
+    assertThat(uri, matchesPattern('repos/.*'))
   })
 
   it('responds with 400 if the connection attempt fails with InvalidRepoUrl', async () => {
-    const repoId = RepoId.of('a-repo-id')
     const remoteUrl = RemoteUrl.of('a-bad-url')
-    app.getInfo.resolves(QueryResult.from())
-    app.connectToRemote.withArgs(repoId, remoteUrl).rejects(new InvalidRepoUrl())
-    const response = await request.post('/repos').send({ repoId: repoId.value, remoteUrl }).expect(400)
+    app.connectToRemote.withArgs(sinon.match.any, remoteUrl).rejects(new InvalidRepoUrl())
+    const response = await request.post('/repos').send({ remoteUrl }).expect(400)
     assertThat(response.text, equalTo(`No git repository found at that URL.`))
   })
 
   it('responds with 403 if the connection attempt fails with AccessDenied', async () => {
-    const repoId = RepoId.of('a-repo-id')
     const remoteUrl = RemoteUrl.of('a-bad-url')
-    app.getInfo.resolves(QueryResult.from())
-    app.connectToRemote.withArgs(repoId, remoteUrl).rejects(new AccessDenied('Sorry!'))
-    const response = await request.post('/repos').send({ repoId: repoId.value, remoteUrl }).expect(403)
+    app.connectToRemote.withArgs(sinon.match.any, remoteUrl).rejects(new AccessDenied('Sorry!'))
+    const response = await request.post('/repos').send({ remoteUrl }).expect(403)
     assertThat(response.text, equalTo(`Access denied: Sorry!`))
   })
 
   it('responds with 500 for any other type of error', async () => {
-    const repoId = RepoId.of('a-repo-id')
     const remoteUrl = RemoteUrl.of('a-bad-url')
-    app.getInfo.resolves(QueryResult.from())
-    app.connectToRemote.withArgs(repoId, remoteUrl).rejects(new Error('unexpected'))
-    const response = await request.post('/repos').send({ repoId: repoId.value, remoteUrl }).expect(500)
+    app.connectToRemote.withArgs(sinon.match.any, remoteUrl).rejects(new Error('unexpected'))
+    const response = await request.post('/repos').send({ remoteUrl }).expect(500)
     assertThat(response.text, equalTo('Internal Server Error'))
   })
 
@@ -87,10 +69,7 @@ describe('POST /repos', () => {
     it('responds with 400 (invalid request) when the request body is malformed', async () => {
       const connectRepoRequest = { repoId: 1 }
       const response = await request.post('/repos').send(connectRepoRequest).expect(400)
-      assertThat(
-        response.text,
-        equalTo("payload should have required property 'remoteUrl', payload/repoId should be string")
-      )
+      assertThat(response.text, equalTo("payload should have required property 'remoteUrl'"))
     })
   })
 })
--- packages/web/src/routes/repos/create.ts
+++ packages/web/src/routes/repos/create.ts
@@ -1,5 +1,6 @@
 import Router from '@koa/router'
-import { Application, RemoteUrl, RepoId, RepoSnapshot } from 'git-en-boite-core'
+import { Application, RemoteUrl, RepoId } from 'git-en-boite-core'
+import LinkHeader from 'http-link-header'
 import { Context } from 'koa'
 
 import validateRequestBody from '../../validate_request'
@@ -9,16 +10,14 @@ const schema = {
   type: 'object',
   title: 'request-payload',
   properties: {
-    repoId: { type: 'string' },
     remoteUrl: { type: 'string' },
   },
-  required: ['repoId', 'remoteUrl'],
+  required: ['remoteUrl'],
 }
 
-type ParsedBody = { repoId: RepoId; remoteUrl: RemoteUrl }
+type ParsedBody = { remoteUrl: RemoteUrl }
 
 const parseBody: (body: any) => ParsedBody = (body: any) => ({
-  repoId: RepoId.fromJSON(body.repoId),
   remoteUrl: RemoteUrl.fromJSON(body.remoteUrl),
 })
 
@@ -29,20 +28,13 @@ export default (app: Application, router: Router): Router =>
     handleRepoErrors,
     async (ctx: Context) => {
       const parsedBody: ParsedBody = parseBody(ctx.request.body)
-      const { repoId, remoteUrl } = parsedBody
-      const result = await app.getInfo(repoId)
-      await result.respond({
-        foundOne: redirectToExisting,
-        foundNone: connect,
-      })
-
-      async function connect() {
-        await app.connectToRemote(repoId, remoteUrl)
-        ctx.response.status = 202
-      }
-
-      async function redirectToExisting(repoInfo: RepoSnapshot) {
-        ctx.response.redirect(router.url('get-repo', { repoId: repoInfo.repoId.urlEncode() }))
-      }
+      const { remoteUrl } = parsedBody
+      const repoId = RepoId.generate()
+      await app.connectToRemote(repoId, remoteUrl)
+      ctx.response.status = 201
+      ctx.response.set(
+        'Link',
+        new LinkHeader().set({ rel: 'item', uri: router.url('get-repo', { repoId: repoId.urlEncode() }) }).toString()
+      )
     }
   )
