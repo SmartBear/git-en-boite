a7108122f3516ed4d729ec24629183bb92d26ac6
Matt Wynne <matt@mattwynne.net>
2021-03-11 21:00:32 -0800

    Rename endpoint handler to use a more conventional name

--- packages/web/src/routes/repos/router.ts
+++ packages/web/src/routes/repos/router.ts
@@ -1,7 +1,7 @@
 import { Application } from 'git-en-boite-core'
 import Router from '@koa/router'
 import create from './create'
-import get from './get'
+import show from './show'
 import update from './update'
 import { buildHandlers } from '../../build_handlers'
 import branches from './branches/router'
@@ -11,7 +11,7 @@ import fetches from './fetches/router'
 
 export default (app: Application, parentRouter: Router): Router =>
   new Router()
-    .use(...buildHandlers([get, create, update], app, parentRouter))
+    .use(...buildHandlers([show, create, update], app, parentRouter))
     .use('/:repoId/commits', commits(app, parentRouter).routes(), commits(app, parentRouter).allowedMethods())
     .use('/:repoId/branches', branches(app, parentRouter).routes(), branches(app, parentRouter).allowedMethods())
     .use('/:repoId/events', events(app, parentRouter).routes(), events(app, parentRouter).allowedMethods())
