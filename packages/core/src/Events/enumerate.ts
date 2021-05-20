633c1ca5ec471cc7de78df1cd0d6875f3e1b5be8
cbohiptest <celine.bon@hiptest.net>
2021-05-20 09:53:10 +0200

    Fix linter in core

--- packages/core/src/Events/enumerate.ts
+++ packages/core/src/Events/enumerate.ts
@@ -1,6 +1,10 @@
+/* eslint-disable prettier/prettier */
 // Utility types for creating a type-checked exhaustive list of the keys in a type at runtime
 // See https://github.com/Microsoft/TypeScript/issues/13298#issuecomment-654906323
 type ValueOf<T> = T[keyof T]
 type NonEmptyArray<T> = [T, ...T[]]
 type MustInclude<T, U extends T[]> = [T] extends [ValueOf<U>] ? U : never
-export const enumerate = <T>() => <U extends NonEmptyArray<T>>(...elements: MustInclude<T, U>) => elements
+export const enumerate =
+  <T>() =>
+  <U extends NonEmptyArray<T>>(...elements: MustInclude<T, U>) =>
+    elements
--- packages/core/src/serialize_errors.ts
+++ packages/core/src/serialize_errors.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { WriteLogEvent } from '.'
 
 type ErrorEnvelope = {
@@ -19,27 +20,26 @@ type ErrorConstructor = {
   new (message?: string): Error
 }
 
-export const buildDeserializeError = (...constructors: Array<ErrorConstructor>) => (
-  anError: Error,
-  log: WriteLogEvent
-): Error => {
-  if (!hasSerializedMessage(anError)) return anError
-  const errorEnvelope: ErrorEnvelope = JSON.parse(anError.message)
-  const Constructor = constructors.find((constructor) => constructor.name === errorEnvelope.type)
-  if (!Constructor) {
-    log({
-      level: 'warn',
-      message: `Unable to properly deserialize an Error of type: ${errorEnvelope.type}. Add the constructor to ${__filename}. Returning as a regular Error for now.`,
-    })
-  }
-  return Object.assign(new (Constructor || Error)(), errorEnvelope.props)
+export const buildDeserializeError =
+  (...constructors: Array<ErrorConstructor>) =>
+  (anError: Error, log: WriteLogEvent): Error => {
+    if (!hasSerializedMessage(anError)) return anError
+    const errorEnvelope: ErrorEnvelope = JSON.parse(anError.message)
+    const Constructor = constructors.find((constructor) => constructor.name === errorEnvelope.type)
+    if (!Constructor) {
+      log({
+        level: 'warn',
+        message: `Unable to properly deserialize an Error of type: ${errorEnvelope.type}. Add the constructor to ${__filename}. Returning as a regular Error for now.`,
+      })
+    }
+    return Object.assign(new (Constructor || Error)(), errorEnvelope.props)
 
-  function hasSerializedMessage(error: Error) {
-    try {
-      return JSON.parse(error.message) instanceof Object
-    } catch (parseError) {
-      if (parseError instanceof SyntaxError) return false
-      throw parseError
+    function hasSerializedMessage(error: Error) {
+      try {
+        return JSON.parse(error.message) instanceof Object
+      } catch (parseError) {
+        if (parseError instanceof SyntaxError) return false
+        throw parseError
+      }
     }
   }
-}
