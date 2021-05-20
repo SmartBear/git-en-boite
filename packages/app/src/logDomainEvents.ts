c7114dd37bc8880a6b711cec4d3a1c08f0538e5a
cbohiptest <celine.bon@hiptest.net>
2021-05-20 09:35:49 +0200

    Fix lint error in app

--- packages/app/src/logDomainEvents.ts
+++ packages/app/src/logDomainEvents.ts
@@ -1,7 +1,9 @@
 import { DomainEvents, DomainRule, WriteLogEvent } from 'git-en-boite-core'
 
-export const logDomainEvents = (log: WriteLogEvent): DomainRule => (events) => {
-  for (const eventName of DomainEvents.names) {
-    events.on(eventName, (event) => log({ level: 'info', message: eventName, event }))
+export const logDomainEvents =
+  (log: WriteLogEvent): DomainRule =>
+  (events) => {
+    for (const eventName of DomainEvents.names) {
+      events.on(eventName, (event) => log({ level: 'info', message: eventName, event }))
+    }
   }
-}
